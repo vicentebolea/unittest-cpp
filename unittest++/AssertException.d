10ab22dedc26d88fa7682134e98ff4e4e662ae49
vicente <vicente@vicente-Aspire-5755G.(none)>
2013-09-10 15:45:13 +0900

    first commit

---
+++ unittest++/AssertException.d
@@ -0,0 +1,24 @@
+src/AssertException.o:  src/AssertException.cpp src/AssertException.h \
+ /usr/include/c++/4.6/exception \
+ /usr/include/c++/4.6/i686-linux-gnu/./bits/c++config.h \
+ /usr/include/c++/4.6/i686-linux-gnu/./bits/os_defines.h \
+ /usr/include/features.h /usr/include/i386-linux-gnu/bits/predefs.h \
+ /usr/include/i386-linux-gnu/sys/cdefs.h \
+ /usr/include/i386-linux-gnu/bits/wordsize.h \
+ /usr/include/i386-linux-gnu/gnu/stubs.h \
+ /usr/include/i386-linux-gnu/gnu/stubs-32.h \
+ /usr/include/c++/4.6/i686-linux-gnu/./bits/cpu_defines.h \
+ /usr/include/c++/4.6/cstring /usr/include/string.h \
+ /usr/lib/gcc/i686-linux-gnu/4.6/include/stddef.h /usr/include/xlocale.h
+src/AssertException.cpp src/AssertException.h :
+ /usr/include/c++/4.6/exception :
+ /usr/include/c++/4.6/i686-linux-gnu/./bits/c++config.h :
+ /usr/include/c++/4.6/i686-linux-gnu/./bits/os_defines.h :
+ /usr/include/features.h /usr/include/i386-linux-gnu/bits/predefs.h :
+ /usr/include/i386-linux-gnu/sys/cdefs.h :
+ /usr/include/i386-linux-gnu/bits/wordsize.h :
+ /usr/include/i386-linux-gnu/gnu/stubs.h :
+ /usr/include/i386-linux-gnu/gnu/stubs-32.h :
+ /usr/include/c++/4.6/i686-linux-gnu/./bits/cpu_defines.h :
+ /usr/include/c++/4.6/cstring /usr/include/string.h :
+ /usr/lib/gcc/i686-linux-gnu/4.6/include/stddef.h /usr/include/xlocale.h :
---
+++ unittest++/ReportAssert.d
@@ -0,0 +1,20 @@
+src/ReportAssert.o:  src/ReportAssert.cpp src/AssertException.h \
+ /usr/include/c++/4.6/exception \
+ /usr/include/c++/4.6/i686-linux-gnu/./bits/c++config.h \
+ /usr/include/c++/4.6/i686-linux-gnu/./bits/os_defines.h \
+ /usr/include/features.h /usr/include/i386-linux-gnu/bits/predefs.h \
+ /usr/include/i386-linux-gnu/sys/cdefs.h \
+ /usr/include/i386-linux-gnu/bits/wordsize.h \
+ /usr/include/i386-linux-gnu/gnu/stubs.h \
+ /usr/include/i386-linux-gnu/gnu/stubs-32.h \
+ /usr/include/c++/4.6/i686-linux-gnu/./bits/cpu_defines.h
+src/ReportAssert.cpp src/AssertException.h :
+ /usr/include/c++/4.6/exception :
+ /usr/include/c++/4.6/i686-linux-gnu/./bits/c++config.h :
+ /usr/include/c++/4.6/i686-linux-gnu/./bits/os_defines.h :
+ /usr/include/features.h /usr/include/i386-linux-gnu/bits/predefs.h :
+ /usr/include/i386-linux-gnu/sys/cdefs.h :
+ /usr/include/i386-linux-gnu/bits/wordsize.h :
+ /usr/include/i386-linux-gnu/gnu/stubs.h :
+ /usr/include/i386-linux-gnu/gnu/stubs-32.h :
+ /usr/include/c++/4.6/i686-linux-gnu/./bits/cpu_defines.h :
